7e6a4afe58ecd68cdb20b5030ff2e402997f4626
sundaramrap <pragya.learn.28@gmail.com>
2025-06-11 19:50:49 +0530

    CounterApplication

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>Counter Value</h1>
+      <button>Add Value</button> <button>Remove Value </button>
+      <p>footer :</p>
+    </>
+  );
+}
+
+export default App;
