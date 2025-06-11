d5bbae115ad79e3b365e10dac1fe5b3d8f77aa87
sundaramrap <pragya.learn.28@gmail.com>
2025-06-11 20:38:25 +0530

    stateUpdated

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 function App() {
+  // let counter = 13;
+
+  const [counter, setCounter] = useState(13);
+
+  //method addValue created.
+  const addValue = () => {
+    // counter = counter + 1;
+    // console.log(counter);
+    setCounter(counter + 1);
+  };
+  //method removeValue created.
+  const removeValue = () => {
+    // counter = counter - 1;
+    // console.log(counter);
+    setCounter(counter - 1);
+  };
   return (
     <>
-      <h1>Counter Value</h1>
-      <button>Add Value</button> <button>Remove Value </button>
-      <p>footer :</p>
+      <h1>Counter Value : {counter}</h1>
+      <button onClick={addValue}>Add Value</button>{" "}
+      <button onClick={removeValue}>Remove Value </button>
+      <p>footer :{counter}</p>
     </>
   );
 }
 
 export default App;
+/**
+ * Want to have a value and change its val from the buttons provided.
+ * STATE
+ * Any change in state  will render the webpage
+ *
+ */
